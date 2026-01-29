615ec61529addf1b3cdb41b426e7f800d3a48c84
madoyakimberley <kimmadoya06@gmail.com>
2026-01-29 09:50:59 +0300

    feat: add routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // Note: router-dom
 import { useJobs } from "./hooks/useJobs";
-
+//components
 import Header from "./components/Header";
-import RoleSelectionScreen from "./pages/RoleSelectionScreen";
-import JobMarketplace from "./pages/JobMarketplace";
-import JobDetailsScreen from "./pages/JobDetailsScreen";
-import WorkerJobFeed from "./pages/WorkerJobFeed";
 import PostTaskModal from "./components/PostTaskModal";
 import PlaceBidModal from "./components/PlaceBidModal";
 import PaymentModal from "./components/PaymentModal";
+//pages
+import RoleSelectionScreen from "./pages/RoleSelectionScreen";
+import JobMarketplace from "./pages/JobMarketplace";
+import WorkerJobFeed from "./pages/WorkerJobFeed";
+import JobDetailsScreen from "./pages/JobDetailsScreen";
 
 function App() {
-  const jobsData = useJobs();
+  const { jobs, loading } = useJobs();
 
-  const [view, setView] = useState("landing");
-  const [userType, setUserType] = useState(null);
-
-  const handleRoleSelect = (role) => {
-    setUserType(role);
-    setView("feed");
-  };
-  //PAGES WILL BE ADDED LATER ON
   return (
-    <div className="app-container">
-      <Header />
+    <BrowserRouter>
+      <div className="app-container min-h-screen bg-slate-50">
+        <Header />
 
-      <PostTaskModal />
-      <PlaceBidModal />
-      <PaymentModal />
-    </div>
+        <main className="max-w-7xl mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<RoleSelectionScreen />} />
+
+            <Route path="/feed" element={<WorkerJobFeed jobs={jobs} />} />
+
+            <Route
+              path="/marketplace"
+              element={<JobMarketplace jobs={jobs} />}
+            />
+
+            <Route path="/job/:id" element={<JobDetailsScreen />} />
+
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+
+        <PostTaskModal />
+        <PlaceBidModal />
+        <PaymentModal />
+      </div>
+    </BrowserRouter>
   );
 }
 
